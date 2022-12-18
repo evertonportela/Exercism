a07c7ca10eb4a880f33b6009fd0e4cee8bb99ba5
Everton P <evertonportela@outlook.com>
2022-12-17 23:14:33 -0300

    exercises swift level 01, level 02

---
+++ swift/lasagna/Sources/Lasagna/Lasagna.swift
@@ -0,0 +1,14 @@
+// TODO: define the 'expectedMinutesInOven' constant
+let expectedMinutesInOven = 40
+// TODO: define the 'remainingMinutesInOven' function
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    return elapsedMinutes - expectedMinutesInOven
+}
+// TODO: define the 'preparationTimeInMinutes' function
+func preparationTimeInMinutes(layers: Int) -> Int {
+    return 2 * layers
+}
+// TODO: define the 'totalTimeInMinutes' function
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    return 2 * layers + elapsedMinutes
+}
