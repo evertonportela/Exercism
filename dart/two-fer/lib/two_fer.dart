46e885436ba6a7e2ceb2f16994534eaa6846c782
Everton P <evertonportela@outlook.com>
2022-12-17 23:10:50 -0300

    exercises dart, level 01, level 02

---
+++ dart/two-fer/lib/two_fer.dart
@@ -0,0 +1,9 @@
+String twoFer([String? text]) {
+  // Replace the throw call and put your code here
+  if (text != null){
+    return 'One for $text, one for me.';
+  }
+  else {
+    return 'One for you, one for me.';
+  }
+}
